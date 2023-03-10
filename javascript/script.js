7ed74700c97fbc4ea5b5a62bafca66f0f7764439
tomasz.tchorzewski@me.com <tomasz.tchorzewski@me.com>
2023-03-10 09:27:55 +0100

    Zmiany powtórne wprowadzenie

---
+++ javascript/script.js
@@ -0,0 +1,78 @@
+// let name1 = 'Tomasz';
+
+// name1 = 'Kamila';
+
+// let number1 = 108;
+
+// let boolean = true;
+
+// let array = [1, 2, 3, 108];
+
+// let objectH1 = document.querySelector('h1');
+
+// objectH1.innerText = "kurs JS";
+
+
+// // if (boolean) {
+
+// // console.log("Warunek spełniony");
+// // }
+
+// if (number1 === 108) {
+
+//     console.log("Warunek spełniony number1 === 108");
+//     }
+    
+// else if (number1 === 110) {
+
+//     console.log("Warunek spełniony number1 === 110");
+//     }
+//     else {
+
+//         console.log("Żaden z warunków nie został spełniony");
+//         }
+
+// let result = number1 * 3;
+
+
+// console.log(result) 
+
+// let numberTxt = '108';
+
+// if (number1 == numberTxt) {
+
+//     console.log("Txt - wartości zmiennych są równe");
+//     }
+
+// let numberTxt2 = '109';
+
+// if (number1 != numberTxt2) {
+
+//     console.log("Txt2 - wartości zmiennych NIE są równe");
+//     }
+
+// if (number1 !== numberTxt) {
+
+//     console.log("Txt - wartości i typy zmiennych NIE są równe");
+//     }
+
+
+// function multiply(num1, num2) {
+
+//     let result = num1 * num2;
+
+//     return result;
+// }
+
+// multiply (2, 3);
+
+// console.log(multiply (2, 3));
+
+// let wynikMnoenia = multiply (number1, 3);
+// console.log
+
+let myHeading = document.querySelector('h1')
+
+myHeading.textContent = 'Kamila i Tomasz'
+myHeading.innerText = 'Wielka Miłość'
+console.log(myHeading)
